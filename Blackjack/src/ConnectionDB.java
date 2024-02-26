7abd5aba728f6da2f01d259423e4430c947a654d
Your Name <you@example.com>
2024-02-26 09:01:27 +0100

    Connection JDBC

--- Blackjack/src/ConnectionDB.java
+++ Blackjack/src/ConnectionDB.java
@@ -1,32 +1,29 @@
 public class ConnectionDB {
-    public class ConnectionDB {
  import java.sql.Connection;
 import java.sql.DriverManager;
 
 
 
-        public class ConnectionDB {
+    public class ConnectionDB {
 
 
-            public static void main(String[] args) {
+        public static void main(String[] args) {
 
-                Connection connection = null;
+            Connection connection = null;
 
-                try {
+            try {
 
-                    Class.forName("org.postgresql.Driver");
-                    connection = DriverManager.getConnection("jdbc:postgresql://Localhost:5432/postgres","postgres","root");
-                    if(connection!= null) {
-                        System.println.out("Connection OK");
-                    } else {
-                        System.println.out("Connection Failed");
-
-                    }
-                } catch (Exception e) {
-
-                    System.println.out(e);
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection("jdbc:postgresql://Localhost:5432/postgres","postgres","root");
+                if(connection!= null) {
+                    System.println.out("Connection OK");
+                } else {
+                    System.println.out("Connection Failed");
 
                 }
+            } catch (Exception e) {
+
+                System.println.out(e);
 
             }
 
